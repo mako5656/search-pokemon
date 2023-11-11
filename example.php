5cac0a7e867beb6c782f731ddedd9b9fcfef6228
MakotoIkami <makoto.ikami@gmail.com>
2023-11-11 19:01:28 +0900

    ♻️ PokeAPIクラスをServiceに移行したので削除

--- example.php
+++ example.php
@@ -4,13 +4,17 @@ declare(strict_types=1);
 
 require_once 'vendor/autoload.php';
 
-use App\Controller\PokeAPI;
+use App\Service\PokeAPI;
 
 $request = new PokeAPI();
 
 //$pokemonInfo = $request->fetchPokemonName('ditto');
-//$pokemonInfo = $request->fetchPokemonId(6);
+$pokemonInfo = $request->fetchPokemonId(6);
 
-$pokemonInfo = $request->fetchPokemon(5, 0);
+//$pokemonInfo = $request->fetchPokemon(5, 0);
+$result = $pokemonInfo['types'];
+//foreach ($result as $key => $value) {
+//    $result[$key] = $request->fetchPokemonName($value['name']);
+//}
 
-var_dump($pokemonInfo);
+var_dump($result);
